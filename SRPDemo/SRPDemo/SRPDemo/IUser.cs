0da201bf43bfced4f422306fcdcef4148a77ad47
Anushil Raman <53249324+AnushilRaman@users.noreply.github.com>
2024-07-18 00:29:01 +0530

    Single Responsibility Principle
    Single Responsibility Principle of SOLID Principle

---
+++ SRPDemo/SRPDemo/SRPDemo/IUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRPDemo
+{
+    public interface IUser
+    {
+        bool Login(string username, string password);
+        bool Register(string username, string password, string email);
+    }
+    public interface ILogger
+    {
+        bool LogError(string error);
+    }
+    public interface IEmail
+    {
+        bool SendEmail(string email);
+    }
+}
